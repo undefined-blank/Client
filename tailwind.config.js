dd5139ec9ded0517cdaf748567178abdcacfb3ee
Duhoon <eclip6@naver.com>
2023-05-26 05:39:27 +0900

    feat: MyPage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
     extend: {},
     colors: {
       primaryColor: '#7FF5E0',
-      ligth10: '#E5F7F7',
+      light10: '#E5F7F7',
       light20: '#CCEFEF',
       light60: '#66D0CE',
       light100: '#00B0AD',
+      profileGray: '#D9D9D9',
+      dashboardGray: '#9FA9B3',
       default: '#0F0F0F',
       descripton: '#CCEFEF',
     }
