33d66d53637002f130b2ba0b02f22dea090a77be
Duhoon <eclip6@naver.com>
2023-05-26 03:13:03 +0900

    feat: Color Name adds

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      primaryColor: '#7FF5E0',
+      ligth10: '#E5F7F7',
+      light20: '#CCEFEF',
+      light60: '#66D0CE',
+      light100: '#00B0AD',
+      default: '#0F0F0F',
+      descripton: '#CCEFEF',
+    }
   },
   plugins: [],
 }
